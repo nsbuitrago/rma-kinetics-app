1ffe7c95c9fbdbcd0d98b5693b6a467a696232d0
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2026-01-26 12:52:41 -0600

    chore: add github pages ci

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,18 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+const dev = process.env.NODE_ENV === "development";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: "404.html", // SPA fallback for client-side routing
+    }),
+    paths: {
+      base: dev ? "" : "/rma-kinetics-app",
+    },
   },
 };
 
